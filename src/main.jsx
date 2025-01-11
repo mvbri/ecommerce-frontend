a3a3ebce57e11903c53e961d0e8be5f845b749b6
Maria Briceño <mvbri10@gmail.com>
2025-01-10 20:45:49 -0400

    Primer commit: subiendo proyecto inicial

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./routes/Signup.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
+import Login from "./routes/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider  router={router}/>
+  </StrictMode>
+);
