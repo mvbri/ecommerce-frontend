cd2a330768e1b326041d9ff2b336db9d33d00369
Maria Briceño <mvbri10@gmail.com>
2025-02-19 19:20:23 -0400

    Feat: conecta el backend para la vista del Login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,19 +1,58 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthProvider";
 import DefaultLayout from "../layout/DefaultLayout";
 import { useState } from "react";
+import { API_URL } from "../auth/constants";
 
 function Login() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [errorResponse, setErrorResponse] = useState();
   const auth = useAuth();
+  const goTo = useNavigate();
 
   if (auth.isAuthenticated) return <Navigate to="/dashboard" />;
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      let response = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        console.log("Something Went Wrong");
+        const json = await response.json();
+        setErrorResponse(json.body.error);
+
+        return;
+      }
+      setErrorResponse("");
+
+      goTo("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <DefaultLayout>
-      <form className="flex justify-center w-max mx-auto items-center flex-col min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center w-max mx-auto items-center flex-col min-h-screen"
+      >
         <h1 className="mb-20">Login</h1>
+
+        {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
+
         <label className="mb-2.5 w-full text-left font-semibold">
           Nombre de usuario
         </label>
