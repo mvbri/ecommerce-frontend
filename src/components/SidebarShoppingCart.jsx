17a04938b0448c4ed9c01cf2bcff1300ef86b9b9
Maria Briceño <mvbri10@gmail.com>
2025-05-11 22:12:28 -0400

    Feat: Adapta nuevo carrito de compras

--- src/components/SidebarShoppingCart.jsx
+++ src/components/SidebarShoppingCart.jsx
@@ -8,36 +8,11 @@ import {
   DialogTitle,
 } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
+import { useCart } from "../hooks/useCart";
+import { API_URL } from "../auth/constants";
 
 export default function SidebarShoppingCart({ open, setOpen }) {
+  const { cart, delFromCart, total } = useCart();
   return (
     <Dialog open={open} onClose={setOpen} className="relative z-[9999]">
       <DialogBackdrop
@@ -56,7 +31,7 @@ export default function SidebarShoppingCart({ open, setOpen }) {
                 <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                   <div className="flex items-start justify-between">
                     <DialogTitle className="text-lg font-medium text-gray-900">
-                      Shopping cart
+                      Carrito
                     </DialogTitle>
                     <div className="ml-3 flex h-7 items-center">
                       <button
@@ -77,45 +52,49 @@ export default function SidebarShoppingCart({ open, setOpen }) {
                         role="list"
                         className="-my-6 divide-y divide-gray-200"
                       >
-                        {products.map((product) => (
-                          <li key={product.id} className="flex py-6">
-                            <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                              <img
-                                alt={product.imageAlt}
-                                src={product.imageSrc}
-                                className="size-full object-cover"
-                              />
-                            </div>
-
-                            <div className="ml-4 flex flex-1 flex-col">
-                              <div>
-                                <div className="flex justify-between text-base font-medium text-gray-900">
-                                  <h3>
-                                    <a href={product.href}>{product.name}</a>
-                                  </h3>
-                                  <p className="ml-4">{product.price}</p>
-                                </div>
-                                <p className="mt-1 text-sm text-gray-500">
-                                  {product.color}
-                                </p>
+                        {cart.length > 0 ? (
+                          cart.map((product) => (
+                            <li key={product.id} className="flex py-6">
+                              <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                <img
+                                  alt={product.name}
+                                  src={`${API_URL}/public/images/products/${product.images[0].url}`}
+                                  className="size-full object-cover"
+                                />
                               </div>
-                              <div className="flex flex-1 items-end justify-between text-sm">
-                                <p className="text-gray-500">
-                                  Qty {product.quantity}
-                                </p>
 
-                                <div className="flex">
-                                  <button
-                                    type="button"
-                                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                                  >
-                                    Remove
-                                  </button>
+                              <div className="ml-4 flex flex-1 flex-col">
+                                <div>
+                                  <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <h3>
+                                      <a href={product.slug}>{product.name}</a>
+                                    </h3>
+                                    <p className="ml-4">{product.price}$</p>
+                                  </div>
+                                </div>
+                                <div className="flex flex-1 items-end justify-between text-sm">
+                                  <p className="text-gray-500">
+                                    Cantidad: {product.quantity}
+                                  </p>
+
+                                  <div className="flex">
+                                    <button
+                                      onClick={() =>
+                                        delFromCart(product._id, true)
+                                      }
+                                      type="button"
+                                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                                    >
+                                      Eliminar
+                                    </button>
+                                  </div>
                                 </div>
                               </div>
-                            </div>
-                          </li>
-                        ))}
+                            </li>
+                          ))
+                        ) : (
+                          <p>No se han agregado productos al carrito.</p>
+                        )}
                       </ul>
                     </div>
                   </div>
@@ -124,17 +103,17 @@ export default function SidebarShoppingCart({ open, setOpen }) {
                 <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                   <div className="flex justify-between text-base font-medium text-gray-900">
                     <p>Subtotal</p>
-                    <p>$262.00</p>
+                    <p>{total}</p>
                   </div>
                   <p className="mt-0.5 text-sm text-gray-500">
-                    Shipping and taxes calculated at checkout.
+                    Impuestos y envíos se calculan al proceder al pago.
                   </p>
                   <div className="mt-6">
                     <a
                       href="#"
                       className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-xs hover:bg-indigo-700"
                     >
-                      Checkout
+                      Proceder al pago
                     </a>
                   </div>
                   <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
@@ -145,7 +124,7 @@ export default function SidebarShoppingCart({ open, setOpen }) {
                         onClick={() => setOpen(false)}
                         className="font-medium text-indigo-600 hover:text-indigo-500"
                       >
-                        Continue Shopping
+                        Continuar Comprando
                         <span aria-hidden="true"> &rarr;</span>
                       </button>
                     </p>
