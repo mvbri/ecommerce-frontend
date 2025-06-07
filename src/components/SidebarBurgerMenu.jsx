7ee749d8d019bebfefee0e9be0f64c6ca1fb62c9
Maria Briceño <mvbri10@gmail.com>
2025-06-07 19:35:51 -0400

    Feat: Implementa botón para generar respaldos

--- src/components/SidebarBurgerMenu.jsx
+++ src/components/SidebarBurgerMenu.jsx
@@ -71,8 +71,9 @@ export function SidebarBurgerMenu() {
               icon={
                 <ChevronDownIcon
                   strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""
-                    }`}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 1 ? "rotate-180" : ""
+                  }`}
                 />
               }
             >
@@ -134,8 +135,9 @@ export function SidebarBurgerMenu() {
               icon={
                 <ChevronDownIcon
                   strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""
-                    }`}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 1 ? "rotate-180" : ""
+                  }`}
                 />
               }
             >
@@ -202,8 +204,9 @@ export function SidebarBurgerMenu() {
               icon={
                 <ChevronDownIcon
                   strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 3 ? "rotate-180" : ""
-                    }`}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 3 ? "rotate-180" : ""
+                  }`}
                 />
               }
             >
@@ -265,8 +268,9 @@ export function SidebarBurgerMenu() {
               icon={
                 <ChevronDownIcon
                   strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 4 ? "rotate-180" : ""
-                    }`}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 4 ? "rotate-180" : ""
+                  }`}
                 />
               }
             >
@@ -317,8 +321,9 @@ export function SidebarBurgerMenu() {
               icon={
                 <ChevronDownIcon
                   strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 5 ? "rotate-180" : ""
-                    }`}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 5 ? "rotate-180" : ""
+                  }`}
                 />
               }
             >
@@ -339,7 +344,7 @@ export function SidebarBurgerMenu() {
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
+                        d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z"
                       />
                     </svg>
                   </ListItemPrefix>
@@ -370,6 +375,70 @@ export function SidebarBurgerMenu() {
                         <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                       </ListItemPrefix>
                       Ver Respaldos
+                    </Link>
+                  </ListItem>
+                </List>
+              </AccordionBody>
+            </Accordion>
+            <Accordion
+              open={open === 6}
+              icon={
+                <ChevronDownIcon
+                  strokeWidth={2.5}
+                  className={`mx-auto h-4 w-4 transition-transform ${
+                    open === 6 ? "rotate-180" : ""
+                  }`}
+                />
+              }
+            >
+              <ListItem className="p-0" selected={open === 6}>
+                <AccordionHeader
+                  onClick={() => handleOpen(6)}
+                  className="border-b-0 p-3"
+                >
+                  <ListItemPrefix>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                      />
+                    </svg>
+                  </ListItemPrefix>
+                  <Typography color="blue-gray" className="mr-auto font-normal">
+                    Pago Móvil
+                  </Typography>
+                </AccordionHeader>
+              </ListItem>
+              <AccordionBody className="py-1">
+                <List className="p-0">
+                  <ListItem>
+                    <Link
+                      className="text-gray-600 flex justify-center items-center"
+                      to="/admin/pago/registrar"
+                    >
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Registrar pago móvil
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <Link
+                      className="text-gray-600 flex justify-center items-center"
+                      to="/admin/pagos"
+                    >
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      ver métodos de pago
                     </Link>
                   </ListItem>
                 </List>
--- src/routes/ShowBackup.jsx
+++ src/routes/ShowBackup.jsx
@@ -8,76 +8,96 @@ import TableBackup from "../components/TableBackup";
 import { axiosInstance } from "../services/axios.config";
 
 const ShowBackup = () => {
-    const { items, setItems } = useFetchBackups();
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [selectedItemName, setSelectedItemName] = useState(null);
-    const [isOpen, openModal, closeModal] = useModal(false);
+  const { items, setItems } = useFetchBackups();
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItemName, setSelectedItemName] = useState(null);
+  const [isOpen, openModal, closeModal] = useModal(false);
 
-    const handleDeletetItem = (item, name) => {
-        setSelectedItem(item);
-        setSelectedItemName(name);
-        openModal();
-    };
+  const handleDeletetItem = (item, name) => {
+    setSelectedItem(item);
+    setSelectedItemName(name);
+    openModal();
+  };
 
-    const deleteItem = async (id, name) => {
-        console.log(name)
-        try {
-         const res = await axiosInstance 
-                .post(`/api/backup/destroy`, {
-                    file: name
-                })
+  const deleteItem = async (id, name) => {
+    try {
+      const res = await axiosInstance.post(`/api/backup/destroy`, {
+        file: name,
+      });
 
-            if (res.status === 200) {
-                const data = res.data.data
-                setItems(data);
-            } else {
-                throw new Error(`[${res.status}] ERROR en la solicitud`);
-            }
-        } catch (error) {
-            console.log(error)
-        } finally {
-            closeModal();
-        }
+      if (res.status === 200) {
+        const data = res.data.data;
+        setItems(data);
+      } else {
+        throw new Error(`[${res.status}] ERROR en la solicitud`);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      closeModal();
+    }
+  };
 
+  const generateBackup = async () => {
+    try {
+      const res = await axiosInstance.post(`/api/backup/generate`);
 
+      if (!res.status === 200) {
+        throw new Error(`[${res.status}] ERROR en la solicitud`);
+      }
 
-    };
+      alert("Respaldo generado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <AdminLayout>
-            <StandardSection className="pt-[4rem]">
-                <h1 className="text-2xl pt-4 md:text-3xl text-gray-800 text-center mb-8 md:mb-14">
-                    Respaldos
-                </h1>
-                <div className="p-4">
-                    {items.length > 0 ? (
-                        <TableBackup onSelectItem={handleDeletetItem} items={items} />
-                    ) : (
-                        <p className="text-center">No hay respaldos en el sistema</p>
-                    )}
-                    {console.log(selectedItem)}
-                    <Modal isOpen={isOpen} closeModal={closeModal}>
-                        <div className="flex items-center justify-center size-full flex-col">
-                            <h2 className="text-center text-gray-950 mb-8 text-xl font-semibold">
-                                ¿Está seguro que desea elimiar el siguiente Item? <br />
-                                <span>
-                                    {`"${selectedItemName ? selectedItemName.split(",").shift() : ""
-                                        }" `}
-                                </span>
-                            </h2>
-                            <button
-                                className="bg-secondary w-fit py-1 px-4 rounded-md
+  return (
+    <AdminLayout>
+      <StandardSection className="pt-[4rem]">
+        <h1 className="text-2xl pt-4 md:text-3xl text-gray-800 text-center mb-8 md:mb-14">
+          Respaldos
+        </h1>
+        <div className="flex justify-end">
+          <button
+            onClick={generateBackup}
+            className="mb-4 inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-2 px-4 md:px-8 shadow-sm hover:shadow-md bg-red-500 border-red-500 text-slate-50 hover:bg-red-400 hover:border-red-400"
+          >
+            GENERAR RESPALDO
+          </button>
+        </div>
+
+        <div className="p-4">
+          {items.length > 0 ? (
+            <TableBackup onSelectItem={handleDeletetItem} items={items} />
+          ) : (
+            <p className="text-center">No hay respaldos en el sistema</p>
+          )}
+          <Modal isOpen={isOpen} closeModal={closeModal}>
+            <div className="flex items-center justify-center size-full flex-col">
+              <h2 className="text-center text-gray-950 mb-8 text-xl font-semibold">
+                ¿Está seguro que desea elimiar el siguiente Item? <br />
+                <span>
+                  {`"${
+                    selectedItemName ? selectedItemName.split(",").shift() : ""
+                  }" `}
+                </span>
+              </h2>
+              <button
+                className="bg-secondary w-fit py-1 px-4 rounded-md
     hover:bg-secondary-accent text-white"
-                                onClick={() => deleteItem(selectedItem, selectedItemName.split(",").shift())}
-                            >
-                                Eliminar
-                            </button>
-                        </div>
-                    </Modal>
-                </div>
-            </StandardSection>
-        </AdminLayout>
-    )
-}
+                onClick={() =>
+                  deleteItem(selectedItem, selectedItemName.split(",").shift())
+                }
+              >
+                Eliminar
+              </button>
+            </div>
+          </Modal>
+        </div>
+      </StandardSection>
+    </AdminLayout>
+  );
+};
 
-export default ShowBackup
+export default ShowBackup;
